596745c724558a1341f480db6bc9190604ebfc57
BuildTools <unconfigured@null.spigotmc.org>
2023-06-05 18:06:16 +0500

    Fixed scope issue

--- src/main/java/me/none030/mortisheads/heads/CrackShotListener.java
+++ src/main/java/me/none030/mortisheads/heads/CrackShotListener.java
@@ -1,15 +1,12 @@
 package me.none030.mortisheads.heads;
 
 import com.shampaggon.crackshot.events.WeaponScopeEvent;
-import me.none030.mortisheads.MortisHeads;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class CrackShotListener implements Listener {
 
-    private final MortisHeads plugin = MortisHeads.getInstance();
     private final HeadManager headManager;
 
     public CrackShotListener(HeadManager headManager) {
@@ -19,15 +16,10 @@ public class CrackShotListener implements Listener {
     @EventHandler
     public void onScope(WeaponScopeEvent e) {
         Player player = e.getPlayer();
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (e.isZoomIn()) {
-                    headManager.getScoping().add(player);
-                }else {
-                    headManager.getScoping().remove(player);
-                }
-            }
-        }.runTask(plugin);
+        if (e.isZoomIn()) {
+            headManager.getScoping().add(player);
+        } else {
+            headManager.getScoping().remove(player);
+        }
     }
 }
